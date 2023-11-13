3adb51dd61f961cf16dca56e7754b9f497845303
Matthew Ostin <matthewositn@gwu.edu>
2023-11-13 11:42:47 -0500

    creating first file

---
+++ Battleship.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class Battleship{
+    // Create the battleship grid and text fields
+    private JTextField battleshipGrid;
+    private JTextField firebutton;
+    private JTextField Xaxis;
+    private JTextField Yaxis;
+
+    public BattleshipGUI() {
+    }
+}
