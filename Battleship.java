5e7fcc93411cc4d355107fdbb9d1118b1699b3be
Matthew Ostin <matthewositn@gwu.edu>
2023-11-15 16:10:33 -0500

    battleship

--- Battleship.java
+++ Battleship.java
@@ -1,14 +1,27 @@
 import javax.swing.*;
+import java.io.*;
+import java.util.ArrayList;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+
 
 public class Battleship{
     // Create the battleship grid and text fields
     private JTextField battleshipGrid;
-    private JTextField firebutton;
+    private JButton firebutton;
     private JTextField Xaxis;
     private JTextField Yaxis;
 
-    public BattleshipGUI() {
+    public static String nameOfShip[] = {"Submarine","Destroyer","Submarine", "Cruiser","Battleship"};
+    public static int sizeOfShip[] = {2,3,4,5}; // represents size of the ship
+    public static int xyBoardSize[] = {10, 10};
+    
+
+
+
+
+
+    public void BattleshipGUI() {
+        
     }
 }
