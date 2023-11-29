6198d990c1848a8ff188bd0af6dec0fed142debd
Matthew Ostin <matthewositn@gwu.edu>
2023-11-29 15:50:58 -0500

    server

---
+++ battleshipClient.java
@@ -0,0 +1,21 @@
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import javax.swing.*;
+import java.io.*;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.net.*;
+
+public class battleshipClient {
+    private Player thisPlayer;
+    private Socket socket;
+    private BufferedReader Reader;
+    private PrintWriter writer;
+    public boolean connected;
+
+    public battleshipClient(Player thisPlayer, String ip, int port){
+
+
+    };
+
+}
