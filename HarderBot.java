5dec541af38debec4ebafa153d3489effffd3dc7
RohanM-26 <rohanm@gwu.edu>
2023-12-04 20:28:17 -0500

    HarderBot rewrite

--- HarderBot.java
+++ HarderBot.java
@@ -1,50 +1,175 @@
+// import java.util.ArrayList;
+// import java.util.Random;
+
+// public class HarderBot extends Bot {
+
+//     public HarderBot(boolean turn, Board ownBoard, Board oppBoard, String[] shipNames, int[] shipSizes) {
+//         super(turn, ownBoard, oppBoard, shipNames, shipSizes);
+//     }
+
+//     private Random rand = new Random();
+
+
+//     Thread harderbotCode = new Thread(() -> {
+//         placeShips();
+//         while (true) {
+//             try {
+//                 Thread.sleep(1);
+//             } catch (Exception e) {
+//                 System.out.println("FAILURE IN HARDER BOT SLEEP");
+//                 return;
+//             }
+//             if (this.turn) {
+//                 //System.out.println("SUPPOSED TO SIMULATE");
+//                 simulateMove();
+//             }
+//         }
+//     });
+
+
+//     @Override
+//     public void placeShips() {
+//         ArrayList<Ship> shipList = new ArrayList<>();
+
+//         for (int size: sizes) {
+//             Ship nextShip = new Ship()
+//             int size2 = sizes[i];
+//             shipList.add(placeShipStrategically(size));
+//         }
+
+//         this.SetShips(shipList);
+//     }
+
+//     /*private Ship placeShipStrategically(int size) {
+//         while (true) {
+//             int x = rand.nextInt(this.OwnBoard.boardSize[0]);
+//             int y = rand.nextInt(this.OwnBoard.boardSize[1]);
+
+//             boolean rotate = rand.nextBoolean();
+
+//             if (isValidPlacement(x, y, size, rotate)) {
+//                 return placeShipOnBoard(x, y, size, rotate);
+//             }
+//         }
+//     }
+//     */
+
+    
+
+//     @Override
+//     public void simulateMove() {
+//         if (!turn) {
+//             Tile target = bestMove();
+//             boolean breaker = this.fire(target);
+
+//             if (breaker && !target.hasBoat()) {
+//                 this.fired = true;
+//                 this.turn = false;
+//             }
+//         }
+//     }
+
+//     private int turns;
+//     private int hits;
+
+    
+//     public ArrayList<Tile> getHitTiles(){
+//         ArrayList<Tile> hitTiles = new ArrayList<>();
+//         for (int r = 0; r < this.OppBoard.boardSize[0]; r++){
+//             for (int c = 0; c < this.OppBoard.boardSize[1]; c++){
+//                 if (this.OppBoard[r][c]){
+//                     hitTiles.add(this.OppBoard[r][c]);
+//                 }
+
+//             }
+//         }
+//     }
+
+//     private boolean isValidTarget(int x, int y) {
+//         return x >= 0 && x < this.OwnBoard.boardSize[0] &&
+//                y >= 0 && y < this.OwnBoard.boardSize[1] &&
+//                !OppBoard.getTile(x, y).beenHit();
+//     }
+
+
+//     private Tile bestMoveMaker() {
+//         ArrayList<Tile> hitTiles = OppBoard.getHitTiles();
+    
+//         for (Tile hitTile : hitTiles) {
+//             int x = hitTile.getX();
+//             int y = hitTile.getY();
+    
+//             if (isValidTarget(x + 1, y)) {
+//                 return OppBoard.getTile(x + 1, y);
+//             }
+//             if (isValidTarget(x - 1, y)) {
+//                 return OppBoard.getTile(x - 1, y);
+//             }
+//             if (isValidTarget(x, y + 1)) {
+//                 return OppBoard.getTile(x, y + 1);
+//             }
+//             if (isValidTarget(x, y - 1)) {
+//                 return OppBoard.getTile(x, y - 1);
+//             }
+//         }
+    
+//         int a = rand.nextInt(this.OwnBoard.boardSize[0]);
+//         int b = rand.nextInt(this.OwnBoard.boardSize[1]);
+//         return OppBoard.getTile(a, b);
+//     }
+    
+
+// }
+
+
 import java.util.ArrayList;
 import java.util.Random;
 
 public class HarderBot extends Bot {
 
+    private Random rand = new Random();
+    private int turns;
+    private int hits;
+
     public HarderBot(boolean turn, Board ownBoard, Board oppBoard, String[] shipNames, int[] shipSizes) {
         super(turn, ownBoard, oppBoard, shipNames, shipSizes);
+
+        Thread harderbotCode = new Thread(() -> {
+            placeShips();
+            while (true) {
+                try {
+                    Thread.sleep(1);
+                } catch (Exception e) {
+                    System.out.println("FAILURE IN HARDER BOT SLEEP");
+                    return;
+                }
+                if (this.turn) {
+                    simulateMove();
+                }
+            }
+        });
+
+        harderbotCode.start();
     }
 
-    private Random rand = new Random();
-
-
-    Thread harderbotCode = new Thread(() -> {
-        placeShips();
-        while (true) {
-            try {
-                Thread.sleep(1);
-            } catch (Exception e) {
-                System.out.println("FAILURE IN HARDER BOT SLEEP");
-                return;
-            }
-            if (this.turn) {
-                //System.out.println("SUPPOSED TO SIMULATE");
-                simulateMove();
-            }
-        }
-    });
-
-
-    @Override
     public void placeShips() {
         ArrayList<Ship> shipList = new ArrayList<>();
-
-        for (int size: sizes) {
-            Ship nextShip = new Ship()
-            int size2 = sizes[i];
-            shipList.add(placeShipStrategically(size));
+        for (int i = sizes.length - 1; i >= 0; i--) {
+            Ship nextShip = placeShip(sizes[i], names[i]);
+            int count = 1;
+            for (Tile a: nextShip.location) {
+                a.placeBoat(nextShip.shipName, count, nextShip.rotate);
+                count++;
+            }
+            shipList.add(nextShip);
         }
-
         this.SetShips(shipList);
     }
 
-    /*private Ship placeShipStrategically(int size) {
+     /*private Ship placeShipStrategically(int size) {
         while (true) {
             int x = rand.nextInt(this.OwnBoard.boardSize[0]);
             int y = rand.nextInt(this.OwnBoard.boardSize[1]);
-
             boolean rotate = rand.nextBoolean();
 
             if (isValidPlacement(x, y, size, rotate)) {
@@ -54,7 +179,35 @@ public class HarderBot extends Bot {
     }
     */
 
-    
+    private boolean isValidPlacement(int x, int y, int size, boolean rotate) {
+        if (rotate) {
+            for (int i = 0; i < size; i++) {
+                if (OwnBoard.getTile(x + i, y).hasBoat()) {
+                    return false;
+                }
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (OwnBoard.getTile(x, y + i).hasBoat()) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private Ship placeShipOnBoard(int x, int y, int size, boolean rotate, String name) {
+        Tile[] location = new Tile[size];
+        for (int i = 0; i < size; i++) {
+            if (rotate) {
+                location[i] = OwnBoard.getTile(x + i, y);
+            } else {
+                location[i] = OwnBoard.getTile(x, y + i);
+            }
+        }
+        Ship ship = new Ship(size, location, name, rotate);
+        return ship;
+    }
 
     @Override
     public void simulateMove() {
@@ -69,36 +222,13 @@ public class HarderBot extends Bot {
         }
     }
 
-    private int turns;
-    private int hits;
+    private Tile bestMove() {
+        ArrayList<Tile> hitTiles = getHitTiles();
 
-    
-    public ArrayList<Tile> getHitTiles(){
-        ArrayList<Tile> hitTiles = new ArrayList<>();
-        for (int r = 0; r < this.OppBoard.boardSize[0]; r++){
-            for (int c = 0; c < this.OppBoard.boardSize[1]; c++){
-                if (this.OppBoard[r][c].){
-                    hitTiles.add(this.OppBoard[r][c]);
-                }
-
-            }
-        }
-    }
-
-    private boolean isValidTarget(int x, int y) {
-        return x >= 0 && x < this.OwnBoard.boardSize[0] &&
-               y >= 0 && y < this.OwnBoard.boardSize[1] &&
-               !OppBoard.getTile(x, y).beenHit();
-    }
-
-
-    private Tile bestMoveMaker() {
-        ArrayList<Tile> hitTiles = OppBoard.getHitTiles();
-    
         for (Tile hitTile : hitTiles) {
             int x = hitTile.getX();
             int y = hitTile.getY();
-    
+
             if (isValidTarget(x + 1, y)) {
                 return OppBoard.getTile(x + 1, y);
             }
@@ -112,11 +242,28 @@ public class HarderBot extends Bot {
                 return OppBoard.getTile(x, y - 1);
             }
         }
-    
+
         int a = rand.nextInt(this.OwnBoard.boardSize[0]);
         int b = rand.nextInt(this.OwnBoard.boardSize[1]);
         return OppBoard.getTile(a, b);
     }
-    
 
+    private ArrayList<Tile> getHitTiles() {
+        ArrayList<Tile> hitTiles = new ArrayList<>();
+        for (int r = 0; r < this.OppBoard.boardSize[0]; r++) {
+            for (int c = 0; c < this.OppBoard.boardSize[1]; c++) {
+                Tile tile = OppBoard.getTile(r, c);
+                if (tile.hasBoat() && tile.beenHit()) {
+                    hitTiles.add(tile);
+                }
+            }
+        }
+        return hitTiles;
+    }
+
+    private boolean isValidTarget(int x, int y) {
+        return x >= 0 && x < this.OwnBoard.boardSize[0] &&
+                y >= 0 && y < this.OwnBoard.boardSize[1] &&
+                !OppBoard.getTile(x, y).beenHit();
+    }
 }
